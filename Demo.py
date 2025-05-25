e4e960dfc29a1cf15576297b95e3a42ac8cfba04
Manoj-Sh-AI <shmanoj100@gmail.com>
2025-05-25 14:39:55 +0200

    Changed aste

--- Demo.py
+++ Demo.py
@@ -2,6 +2,6 @@ def print_full_name(first, family):
     return first + " " + family
 
 first = "Manoj"
-family = "Saligrama Harisha - doing again ------"
+family = "Saligrama Harisha - doing again & again"
 result = print_full_name(first, family)
 print(result)
